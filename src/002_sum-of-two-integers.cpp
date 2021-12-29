c293361532525c83d075fcc781a731b93e400fa2
Jake Turelli <Jake.Turelli@gmail.com>
2021-12-28 21:51:35 -0800

    start sum of two integers

---
+++ src/002_sum-of-two-integers.cpp
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////
+/// @file 002_sum-of-two-integers.cpp
+/// @author Jake Turelli (jake.turelli@gmail.com)
+/// @brief https://leetcode.com/problems/sum-of-two-integers/
+/// @version 0.1
+/// @date 2021-12-28
+///
+/// @copyright Copyright (c) 2021
+///
+///////////////////////////////////////////////
+
+#include <vector>
+#include <map>
+#include <cstdio>
+using namespace std;
+
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution,
+// and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+class Solution
+{
+public:
+    //Given two integers a and b, return the sum of the two integers without using the operators + and -.
+    int getSum(int a, int b)
+    {
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+
+    return 0;
+}
