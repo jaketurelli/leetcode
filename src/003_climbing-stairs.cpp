89918a13964563fe43906328e046a422e81debe5
Jake Turelli <Jake.Turelli@gmail.com>
2021-12-30 20:01:55 -0800

    adding start to climbing stairs

---
+++ src/003_climbing-stairs.cpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////
+/// @file 003_climbing-stairs.cpp
+/// @author Jake Turelli (jake.turelli@gmail.com)
+/// @brief https://leetcode.com/problems/climbing-stairs/
+/// @version 0.1
+/// @date 2021-12-30
+///
+/// @copyright Copyright (c) 2021
+///
+///////////////////////////////////////////////
+
+// You are climbing a staircase.It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
