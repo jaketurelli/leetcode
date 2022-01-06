c8ee6ec33593a576a50563f0892f419acb6b8901
Jake Turelli <Jake.Turelli@gmail.com>
2022-01-05 22:14:44 -0800

    start insert interval

---
+++ src/005_insert-interval.cpp
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////
+/// @file 005_insert-interval.cpp
+/// @author Jake Turelli (jake.turelli@gmail.com)
+/// @brief https://leetcode.com/problems/insert-interval/
+/// @version 0.1
+/// @date 2022-01-05
+///
+/// @copyright Copyright (c) 2022
+///
+///////////////////////////////////////////////
+#include <vector>
+using namespace std;
+
+// You are given an array of non-overlapping intervals intervals where
+// intervals[i] = [starti, endi] represent the start and the end of
+// the ith interval and intervals is sorted in ascending order by starti.
+// You are also given an interval newInterval = [start, end] that
+// represents the start and end of another interval.
+
+// Insert newInterval into intervals such that intervals is still sorted
+// in ascending order by starti and intervals still does not have any
+// overlapping intervals (merge overlapping intervals if necessary).
+
+// Return intervals after the insertion.
+class Solution
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+    {
+    }
+};
